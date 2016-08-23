5a0abbc3532b8fc82e7ac9a7c3be1b9df46ea41e
cosa65 <mfosco65@gmail.com>
2016-08-23 18:38:21 -0300

    ejercicio1 ahora un poco mas cerca de lo que se supone que es

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -1,68 +1,48 @@
 #include <iostream>
 #include <vector>
 
-bool get_best_bag(std::vector<int> D, int N, int P, int& res) {
+void add_possible_subsets(int P, std::vector<int>& xs, int x) {
 
-	bool changed_res = false;
+	int n = xs.size();
 
-	for (int i = 0; i < N; i++) {
-		if (D[i] >= res and D[i] <= P) {
-			res = D[i];
-			changed_res = true;
+	if (x <= P) {
+		xs.push_back(x);	
+		int act;
+		for (int i = 0; i < n; i++) {
+			act = x + xs[i];
+			if (act <= P) {
+				xs.push_back(act);
+			}
 		}
 	}
+}
 
+std::vector<int> combine(int P, std::vector<int> X, std::vector<int> Y) {
+
+	vector<int> subsX, subsY;
+	int n = X.size();
+	int m = Y.size();
+	for (int i = 0; i < n; i++) {
+		add_possible_subsets(P, subsX, X[i]);
+	}
+
+	for (int i = 0; i < m; i++) {
+		add_possible_subsets(P, subsY, Y[i]);
+	}
+
+	int size_subs_x = subsX.size();
+
+	for (int i = 0; i < size_subs_x; i++) {
+		add_possible_subsets(P, subsY, subsX[i]);
+	}
+}
+
+std::vector<int> find_possibles(int P, std::vector<int> D, int start, int end) {
+
+	std::vector<int> X = find_possibles(P, D, start, end/2);
+	std::vector<int> Y = find_possibles(P, D, end/2, end);
+	std::vector<int> res = combine(P, X, Y);
 	return res;
-}
-
-void get_min(std::vector<int>& D, int& min1, int& min2, int N) {
-
-	min1 = 0;
-	min2 = 1;
-
-	if (D[0] > D[1]) std::swap(min1, min2);
-	
-	for (int i = 2; i < N; i++) {
-		if (D[i] < D[min1]) {
-			min2 = min1;
-			min1 = i;
-		}
-	}
-}
-
-void add_and_merge(std::vector<int>& D, int min1, int min2, int& N) {	
- 
-	if (min1 != N-1) {		//Si min1 no es el ultimo elemento puedo mover al ultimo a ese lugar.
-		D[min2] += D[min1];	//Sumo a los dos menores
-		D[min1] = D[N-1];	//Achico en 1 el vector (muevo al ultimo elemento al lugar sobrante de uno de los 2 menores que mergie)
-	} else {
-		D[min1] += D[min2];	//Sumo a los dos menores
-		D[min2] = D[N-1];	//Achico en 1 el vector (muevo al ultimo elemento al lugar sobrante de uno de los 2 menores que mergie)
-	}
-
-	N--;
-}
-
-int best_option(int P, int N, std::vector<int>& D) {
-
-	int min1, min2;
-	int res = 0;
-	bool can_spend_more;
-
-	while (can_spend_more) {
-		can_spend_more = get_best_bag(D, N, P, res);
-		if (N < 2) {
-			return res;
-		}
-		get_min(D, min1, min2, N);
-		if (P < D[min1]) {
-			return res;
-		}
-		add_and_merge(D, min1, min2, N);
-	}
-
-	return res;
-
 }
 
 int main() {
@@ -76,6 +56,6 @@ int main() {
 		std::cin >> D[i];
 	}
 
-	int res = best_option(P, N, D);
+	int res = best_option(P, D, 0, N);
 
 }
