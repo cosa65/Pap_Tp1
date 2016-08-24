288fb69b2c1194d33300c1855be9fcb0c193e277
TomoDC <christian.chi93@gmail.com>
2016-08-23 23:12:04 -0300

    Es asi?? Pinta muy tranca y sospechoso! JAJA

---
+++ ej3/Ej3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int a;
+	int suma = 0;
+	int max = 0;
+	for(int i=0; i<n; i++){
+		cin >> a;
+		suma += a;
+		if(suma > max) max = suma;
+		else if (suma < 0) suma = 0;
+	}
+	cout << max << endl;
+	return 0;
+}
